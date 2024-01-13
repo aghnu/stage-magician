dfae5727baa4b4a8d4bd21947233691279680b23
Gengyuan Huang <46549455+aghnu@users.noreply.github.com>
2024-01-13 11:24:22 -0700

    Init project structure

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+// vite.config.ts
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+// https://vitejs.dev/guide/build.html#library-mode
+export default defineConfig({
+  build: {
+    emptyOutDir: true,
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "stage-magician",
+      fileName: "stage-magician",
+    },
+  },
+  plugins: [
+    dts({
+      exclude: [
+        "./vite.config.ts",
+        "./.eslintrc.cjs",
+        "./jest.config.cjs",
+        "**/*.test.ts",
+      ],
+    }),
+  ],
+});
