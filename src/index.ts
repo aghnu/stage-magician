d52028d3eb69619dbc37ab6519c2af24f2e84e41
Gengyuan Huang <46549455+aghnu@users.noreply.github.com>
2024-01-13 18:11:50 -0700

    feature(useCleanup): expose useCleanup

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,4 @@ export {
 } from "./functions/usePointer.ts";
 export { useAnimationLoop } from "./functions/useAnimation.ts";
 export { useHtmlElementProperty } from "./functions/useHtmlElement.ts";
+export { useCleanup } from "./functions/useCleanup.ts";
